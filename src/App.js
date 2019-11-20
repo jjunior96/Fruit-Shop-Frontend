cbcd297415659423039251bc714892c9685fd119
Junior Alves <jjalves96@gmail.com>
2019-11-20 10:45:44 -0300

    Tela de login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="Fruit-Shop"/>
+      
+      <div className="content">
+
+        <p>Alimentos <strong>saudáveis</strong> para toda sua <strong>família</strong>.</p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor email"
+          />
+          
+          <label htmlFor="email">SENHA *</label>
+          <input 
+            type="password" 
+            id="password" 
+            placeholder="Sua senha"
+          />
+
+          <button className="btn-login" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
