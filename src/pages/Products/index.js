6b7a60ab666a220bc05eabcb5defaf9088d6176c
Junior Alves <jjalves96@gmail.com>
2019-11-22 08:38:13 -0300

    Pagina de produtos

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 export default function Products() {
-  return <div></div>;
+  const [ products, setProducts ] = useState([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const user_id = localStorage.getItem('user');
+      const response = await api.get('/products', {
+        headers: { user_id }
+      });
+
+      setProducts(response.data);
+    }
+
+    loadProducts();
+  }, []);
+  return (
+    <>
+      <ul className="product-list">
+        {products.map(product => (
+          <li key={product._id}>
+            <header></header>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
